23f12895bc918ab4c0f11aec9e38dd5355c2066e
aleksishis <aleksis-his@yandex.ru>
2023-05-17 23:28:37 +0300

    внес правки после второй этерации

--- js/js.js
+++ js/js.js
@@ -3,10 +3,10 @@ const openPopupBtn = document.querySelector(".profile__edit");
 const closedPopupBtn = document.querySelector(".popup__closed");
 
 const profileName = document.querySelector(".profile__name");
-const nameInput = document.querySelector(".popup__input");
+const nameInput = document.querySelector(".popup__input_prompt_name");
 
 const profileJob = document.querySelector(".profile__description")
-const jobInput = document.querySelector(".popup__input_description");
+const jobInput = document.querySelector(".popup__input_prompt_job");
 
 const editForm = document.querySelector(".popup__form");
 
@@ -25,10 +25,10 @@ editForm.addEventListener('submit', function (event) {
 })
 
 function openPopup() {
-  popup.classList.add("popup_is_opened")
+  popup.classList.add("popup_opened")
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 function closePopup() {
-  popup.classList.remove("popup_is_opened")
+  popup.classList.remove("popup_opened")
 }
