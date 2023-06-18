d360695262add403c47968f98805c7ad81abd3a3
aleksishis <aleksis-his@yandex.ru>
2023-06-18 15:58:34 +0300

    делаю валидацию

--- js/js.js
+++ js/js.js
@@ -11,7 +11,7 @@ const nameInput = document.querySelector(".popup__input_prompt_name");
 const profileJob = document.querySelector(".profile__description")
 const jobInput = document.querySelector(".popup__input_prompt_job");
 
-const editForm = document.querySelector(".popup__form");
+const editForm = document.querySelector(".popupProfile__form");
 
 const popupAddCard = document.querySelector(".popupAddCard");
 const openpopupAddCard = document.querySelector(".profile__add");
@@ -68,6 +68,7 @@ closeButtons.forEach((button) => {
 
 function openPopup(popup) {
   popup.classList.add("popup_opened");
+  enableValidation()
 }
 function closePopup(popup) {
   popup.classList.remove("popup_opened");
@@ -79,13 +80,10 @@ openPopupProfileBtn.addEventListener('click', function () {
   openPopup(profilePopup);
 })
 
-
 editForm.addEventListener('submit', function (event) {
   event.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-
-
   //закрываю попап изменения профиля 
   closePopup(profilePopup);
 })
@@ -149,4 +147,113 @@ formPopupAddCardEl.addEventListener('submit', function (event) {
   closePopup(popupAddCard);
 }
 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__error_visible');
+
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__error_visible');
+  errorElement.textContent = '';
+};
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    // Если поле не валидно, колбэк вернёт true
+    // Обход массива прекратится и вся функция
+    // hasInvalidInput вернёт true
+    return !inputElement.validity.valid;
+  })
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('button_inactive');
+  } else {
+    buttonElement.classList.remove('button_inactive');
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+
+  const buttonElement = formElement.querySelector('.popup__button')
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement);
+
+  });
+};
+enableValidation();
+
+
+
+
+//checkInputValidity()
+//toggleButtonState()
+//setEventListeners()
+//hasInvalidInput()
+
+
+//document.getElementById('myForm').addEventListener('submit', function (event) {
+//  let input = document.getElementById('myInput')
+//  let error = document.querySelector('.error')
+
+//  // Очистим сообщение об ошибке перед каждой попыткой отправки
+//  error.textContent = ''
+
+//  if (!input.validity.valid) {
+//    // Если поле не проходит проверку, отображаем соответствующее сообщение об ошибке
+//    if (input.validity.valueMissing) {
+//      error.textContent = "Поле не может быть пустым"
+//    } else if (input.validity.tooShort) {
+//      error.textContent = "Введенное значение должно быть длиной не менее 2 символов"
+//    } else if (input.validity.tooLong) {
+//      error.textContent = "Введенное значение должно быть длиной не более 40 символов"
+//    }
+//    event.preventDefault() // Останавливаем отправку формы
+//  }
+//})
+
 
